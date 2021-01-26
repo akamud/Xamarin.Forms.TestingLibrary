32359e1684938c256136a9b504d414028cf8e559
Mahmoud Ali <muddibr@gmail.com>
2021-01-26 12:07:22 -0300

    WIP

--- src/Xamarin.Forms.TestingLibrary/Screen.cs
+++ src/Xamarin.Forms.TestingLibrary/Screen.cs
@@ -13,18 +13,6 @@ namespace Xamarin.Forms.TestingLibrary
 
         internal Screen(TPage page) => Container = page;
 
-        public View? QueryByText(string text) => FindByText(text).SingleOrDefault();
-        public IReadOnlyCollection<View> QueryAllByText(string text) => FindByText(text).ToList().AsReadOnly();
-        public View GetByText(string text) => FindByText(text).Single();
-        public IReadOnlyCollection<View> GetAllByText(string text)
-        {
-            var foundViews = FindByText(text).ToList();
-
-            return foundViews.Count > 0
-                ? foundViews.AsReadOnly()
-                : throw new InvalidOperationException("Sequence contains no elements");
-        }
-
         private IEnumerable<View> FindByText(string text)
         {
             var foundViews = new List<View>();
@@ -52,6 +40,18 @@ namespace Xamarin.Forms.TestingLibrary
             }
 
             return foundViews;
+        }
+
+        public View? QueryByText(string text) => FindByText(text).SingleOrDefault();
+        public IReadOnlyCollection<View> QueryAllByText(string text) => FindByText(text).ToList().AsReadOnly();
+        public View GetByText(string text) => FindByText(text).Single();
+        public IReadOnlyCollection<View> GetAllByText(string text)
+        {
+            var foundViews = FindByText(text).ToList();
+
+            return foundViews.Count > 0
+                ? foundViews.AsReadOnly()
+                : throw new InvalidOperationException("Sequence contains no elements");
         }
     }
 }
