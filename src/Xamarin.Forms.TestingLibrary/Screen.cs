a0ff2ac51151697fdce2e064c6269da7d566e4ea
Mahmoud Ali <muddibr@gmail.com>
2021-02-12 16:59:56 -0300

    Add GetByType docs

--- src/Xamarin.Forms.TestingLibrary/Screen.cs
+++ src/Xamarin.Forms.TestingLibrary/Screen.cs
@@ -61,7 +61,7 @@ namespace Xamarin.Forms.TestingLibrary
         /// or null if there are no matching elements.
         /// Throws an exception if there is more than one matching element on the screen.
         /// </summary>
-        /// <typeparam name="T">The type of the expected View.</typeparam>
+        /// <typeparam name="T">The type of the expected view.</typeparam>
         /// <param name="text">The text that the view should have on the screen.</param>
         /// <returns>The single <typeparamref name="T"/> matching the <paramref name="text"/>.</returns>
         /// <exception cref="System.ArgumentNullException">
@@ -93,7 +93,7 @@ namespace Xamarin.Forms.TestingLibrary
         /// Returns all <typeparamref name="T"/> on the screen matching the given <paramref name="text"/>
         /// or an empty collection if there are no matching elements.
         /// </summary>
-        /// <typeparam name="T">The type of the expected Views.</typeparam>
+        /// <typeparam name="T">The type of the expected views.</typeparam>
         /// <param name="text">The text that the views should have on the screen.</param>
         /// <returns>A IReadOnlyCollection of views matching the <paramref name="text"/>.</returns>
         /// <exception cref="System.ArgumentNullException">
@@ -118,14 +118,14 @@ namespace Xamarin.Forms.TestingLibrary
         /// Returns the only <typeparamref name="T"/> on the screen matching the given <paramref name="text"/>.
         /// Throws an exception if there is not exactly one matching element on the screen.
         /// </summary>
-        /// <typeparam name="T">The type of the expected View.</typeparam>
+        /// <typeparam name="T">The type of the expected view.</typeparam>
         /// <param name="text">The text that the view should have on the screen.</param>
         /// <returns>The single <typeparamref name="T"/> matching the <paramref name="text"/>.</returns>
         /// <exception cref="System.ArgumentNullException">
         /// Screen does not have a valid view hierarchy.
         /// </exception>
         /// <exception cref="System.InvalidOperationException">
-        /// The screen contains more than one matching element. -or- There are matching elements.
+        /// The screen contains more than one matching element. -or- There are no matching elements.
         /// </exception>
         public T GetByText<T>(string text) where T : View
             => GetBy<T>(x => x.HasTextValueWith(text));
@@ -141,7 +141,7 @@ namespace Xamarin.Forms.TestingLibrary
         /// Screen does not have a valid view hierarchy.
         /// </exception>
         /// <exception cref="System.InvalidOperationException">
-        /// The screen contains more than one matching element. -or- There are matching elements.
+        /// The screen contains more than one matching element. -or- There are no matching elements.
         /// </exception>
         public View GetByText(string text) => GetByText<View>(text);
 
@@ -175,13 +175,59 @@ namespace Xamarin.Forms.TestingLibrary
         /// </exception>
         public IReadOnlyCollection<View> GetAllByText(string text) => GetAllByText<View>(text);
 
+        /// <summary>
+        /// Returns the only view on the screen matching the given type <typeparamref name="T"/>
+        /// or null if there are no matching elements.
+        /// Throws an exception if there is more than one matching element on the screen.
+        /// </summary>
+        /// <typeparam name="T">The type of the expected view.</typeparam>
+        /// <returns>The single view matching the type <typeparamref name="T"/>.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Screen does not have a valid view hierarchy.
+        /// </exception>
+        /// <exception cref="System.InvalidOperationException">
+        /// The screen contains more than one matching element.
+        /// </exception>
         public T? QueryByType<T>() where T : View => QueryBy<T>();
 
+        /// <summary>
+        /// Returns all views matching the type <typeparamref name="T"/> on the screen
+        /// or an empty collection if there are no matching elements.
+        /// </summary>
+        /// <typeparam name="T">The type of the expected views.</typeparam>
+        /// <returns>A IReadOnlyCollection of views matching the type <typeparamref name="T"/>.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Screen does not have a valid view hierarchy.
+        /// </exception>
         public IReadOnlyCollection<T> QueryAllByType<T>() where T : View =>
             QueryAllBy<T>();
 
+        /// <summary>
+        /// Returns the only view matching the type <typeparamref name="T"/> on the screen. 
+        /// Throws an exception if there is not exactly one matching element on the screen.
+        /// </summary>
+        /// <typeparam name="T">The type of the expected view.</typeparam>
+        /// <returns>The single view matching the type <typeparamref name="T"/>.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Screen does not have a valid view hierarchy.
+        /// </exception>
+        /// <exception cref="System.InvalidOperationException">
+        /// The screen contains more than one matching element. -or- There are no matching elements.
+        /// </exception>
         public T GetByType<T>() where T : View => GetBy<T>();
 
+        /// <summary>
+        /// Returns all views matching the type <typeparamref name="T"/> on the screen.
+        /// Throws an exception if there is not at least one matching element on the screen.
+        /// </summary>
+        /// <typeparam name="T">The type of the expected views.</typeparam>
+        /// <returns>A IReadOnlyCollection of views matching the type <typeparamref name="T"/>.</returns>
+        /// <exception cref="System.ArgumentNullException">
+        /// Screen does not have a valid view hierarchy.
+        /// </exception>
+        /// <exception cref="System.InvalidOperationException">
+        /// The screen does not contain at least one matching element.
+        /// </exception>
         public IReadOnlyCollection<T> GetAllByType<T>() where T : View
             => GetAllBy<T>(exceptionMessage: "Sequence contains no elements");
 
